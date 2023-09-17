39def1c6a1db3d1bda186fbd21b17ccb8cf4065e
ThearyanThe <aryan11228sh@gmail.com>
2023-09-17 07:36:17 -0700

    style:End-Of-Uploading-article-formating

--- src/js/sessions.js
+++ src/js/sessions.js
@@ -1,11 +1,11 @@
-import { getAndShowAllSessions,prepareCreateNewSessionForm,createSession,removeSession } from "./func/sessions.js";
-window.removeSession=removeSession
-const SendSessions=document.querySelector("#SendSessions")
-window.addEventListener("load",()=>{
+import { getAndShowAllSessions, prepareCreateNewSessionForm, createSession, removeSession } from "./func/sessions.js";
+window.removeSession = removeSession
+const SendSessions = document.querySelector("#SendSessions")
+window.addEventListener("load", () => {
     getAndShowAllSessions()
     prepareCreateNewSessionForm()
 })
-SendSessions.addEventListener("click",(event)=>{
+SendSessions.addEventListener("click", (event) => {
     event.preventDefault()
-  createSession()
+    createSession()
 })
